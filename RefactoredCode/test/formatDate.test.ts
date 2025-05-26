bb205f73503115c7f8c5b4b72068a636aacd35af
Antonio Russo <antonio.cror@gmail.com>
2025-05-26 13:43:44 -0300

    Added more Test cases to the FormatDate script

--- RefactoredCode/test/formatDate.test.ts
+++ RefactoredCode/test/formatDate.test.ts
@@ -5,10 +5,23 @@ describe('formatDateToInput', () => {
     const testCases = [
         {
             input: {
-                date: '2025-12-25T11:00:00.000Z'
+                date: '2025-12-25T11:00:00.000Z' // Data qualquer
             },
             expected: '2025-12-25'
-        }
+        },
+        {
+            input: { 
+                date: '2020-02-29T00:00:00.000Z' }, // Ano bissexto
+            expected: '2020-02-29'
+        },
+        {
+            input: { date: '2000-12-31T00:00:00.000Z' }, // Final do ano
+            expected: '2000-12-31'
+        },
+        {
+            input: { date: '2022-04-01' }, // Data sem o hora
+            expected: '2022-04-01'
+        },
     ]
 
     for (const testCase of testCases){
