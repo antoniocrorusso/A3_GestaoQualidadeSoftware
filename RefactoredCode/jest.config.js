98ed16ed6d4a25a8c9655b68752b99437bf5396c
Antonio Russo <antonio.cror@gmail.com>
2025-05-26 16:58:59 -0300

    Added the HTML report generator for unit testing and its dependencies.
    Now when the test is run, it should generate a html as report file, which can be opened on any browser, with the test results.

--- RefactoredCode/jest.config.js
+++ RefactoredCode/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
     testMatch: ['**/*.test.ts'],
+    reporters: [
+        "default",
+        ["./node_modules/jest-html-reporter", {
+            pageTitle: "Relatório dos Testes Unitários - A3",
+            includeFailureMsg: true,
+            includConsoleLog: true,
+            sort: 'titleAsc'
+        }]
+    ]
 };
